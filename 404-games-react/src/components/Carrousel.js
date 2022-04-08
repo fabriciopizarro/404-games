62ee477b79ec1cfe06baeab808589cc2c9b45f90
Fabricio Yaringaño <fabricio.pizarro.19@gmail.com>
2022-04-07 19:47:44 -0500

    complementacion de carousel

--- 404-games-react/src/components/Carrousel.js
+++ 404-games-react/src/components/Carrousel.js
@@ -1,32 +1,134 @@
 const Carrousel = () => {
     return ( 
-        <section>
+        <section class="carousel-container" >
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://cdn2.unrealengine.com/Epic+Games+Node%2Frr-1920x1080-5a3f8bce26f45d078c2738cf5140ad18be39041c.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
+                    <img src="https://cdn2.unrealengine.com/Epic+Games+Node%2Fue-alt-1920x1080-e653a4a4dae65307fd2420076abe44bb71b22f06.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://cdn2.unrealengine.com/egs-genshin-impact-2-6-desktop-v2-1248x702-1248x702-7826fd8eb9e9.png?h=1080&resize=1&w=1920" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://cdn2.unrealengine.com/epichomepage-artboard-1-1920x1080-329693365.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://cdn2.unrealengine.com/rl-f2p-hero-epic-adjust-gradient-1920x1080-982990639.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
+                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="https://cdn2.unrealengine.com/Epic+Games+Node%2Fut-1920x1080-416b0b679e572854df52c39154aff5945328c04d.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
+                    <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="https://cdn2.unrealengine.com/Epic+Games+Node%2Fue-alt-1920x1080-e653a4a4dae65307fd2420076abe44bb71b22f06.jpg?resize=1&w=1920" class="d-block w-100" alt="..." />
+                    <img src="https://cdn2.unrealengine.com/egs-lego-sw-skywalker-carousel-desktop-1280x720-ab6e8359c0bc.jpg?h=1080&resize=1&w=1920" class="d-block w-100" alt="..." />
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                    <div class="carousel-play">
+                            <div class="carousel-trailer-container">
+                                    <div class="clase">
+                                    <a class="carousel-trailer">Ver trailer</a>
+                                    </div>
+                                    
+                                    <div>    
+                                    <svg class="carousel-trailer" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="24" cy="24" r="23.5" fill="#000" fill-opacity=".6" stroke="#fff"></circle>
+                                        <path d="M31.869 22.4a2 2 0 010 3.2L22.2 32.856c-1.319.99-3.201.049-3.201-1.6V16.744c0-1.648 1.882-2.589 3.2-1.6L31.87 22.4z" fill="#fff"></path>
+                                    </svg>
+                                    </div>
+                            </div>
+                            <a class="carousel-trailer">Jugar ahora</a>
+                        </div>
                     </div>
                 </div>
             </div>

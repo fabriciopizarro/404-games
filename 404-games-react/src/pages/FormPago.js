f15c9c694c328f2cd3211a4ddf14e9b9f0692ab2
Gino Diez <diezsilvag@gmail.com>
2022-06-21 19:34:47 -0500

    Modificando el formulario de pago

--- 404-games-react/src/pages/FormPago.js
+++ 404-games-react/src/pages/FormPago.js
@@ -8,23 +8,12 @@ const FormPago = () => {
         <section className="form-pago-container">
            <h1 className="form-pago__title">Formato de Pago</h1>
            <form className="formulario-pago">
-               <div className="datos-personales__container">
-                    <input type="text" id="nombre" placeholder="Nombre Completo" name="nombre" className="registro__form-input" required pattern="^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$"
-                    title="No se aceptan caracteres especiales ni números"/>
-                
-                    <input type="text" id="apellido" placeholder="Apellido Completo" name="apellido" className="registro__form-input" required pattern="^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$"
-                    title="No se aceptan caracteres especiales ni números"/>
-               </div>
-                
-                <input type="text" id="email" placeholder="Email" name="email" className="registro__form-input" required />
-                
-                <input type="number" id="celular" placeholder="Celular" name="celular" className="registro__form-input" min="900000000" max="999999999" required />
 
-                <input type="number" id="numCuenta" placeholder="Numero de cuenta" name="numeroCuenta" className="registro__form-input" min="90000000000000" max="99999999999999" required />
+                <input type="number" id="numTarjeta" placeholder="Numero de tarjeta" name="numeroTarjeta" className="registro__form-input" min="90000000000000" max="99999999999999" required />
 
-                <input type="text" id="banco" placeholder="Nombre de Banco" name="banco" className="registro__form-input" required />
+                <input type="text" id="banco" placeholder="Fecha de vencimiento" name="banco" className="registro__form-input" required />
 
-                <input type="number" id="codigoPostal" placeholder="Codigo Postal" name="codigoPostal" className="registro__form-input" min="90000" max="99999" required />
+                <input type="number" id="ccv" placeholder="CCV" name="ccv" className="registro__form-input" min="90000" max="99999" required />
                 
                 <div className="buttons__container">
                     <button className="registro__form-button">Pagar</button>
